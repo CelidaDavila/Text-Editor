8b08bf5c7af0bb68b930607b862208f477415a4d
Celida Davila <celiximena1@gmail.com>
2026-03-22 18:11:21 -0600

    Delete build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_text-editor-app_FILE C:/Users/Usuario/OneDrive/Documentos/Proyectos/Text-Editor/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/text-editor-app.exe)
-set(__QT_DEPLOY_TARGET_text-editor-app_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_text-editor-app_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
